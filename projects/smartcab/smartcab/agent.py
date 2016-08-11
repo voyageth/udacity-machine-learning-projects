04e3b966c2396fdff396b95d3e46b4671bb8faef
voyageth <voyageth@gmail.com>
2016-08-11 09:55:48 +0900

    feat: Implement q-learning
    - add next waypoint to q-values.
    - use q-values when select action.

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -1,3 +1,5 @@
+import random
+
 import numpy as np
 import pandas as pd
 
@@ -16,14 +18,36 @@ class LearningAgent(Agent):
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
         # TODO: Initialize any additional variables here
         self.available_actions = [None, 'forward', 'left', 'right']
-        # TODO add next_waypoint to state.
-        self.available_states = ['GREEN_CAN_LEFT', 'GREEN_CANT_LEFT', 'RED_CAN_RIGHT', 'RED_CANT_RIGHT']
+        self.available_states = [
+            'GREEN_CAN_LEFT_NEXT_NONE'
+            , 'GREEN_CAN_LEFT_NEXT_FORWARD'
+            , 'GREEN_CAN_LEFT_NEXT_LEFT'
+            , 'GREEN_CAN_LEFT_NEXT_RIGHT'
+
+            , 'GREEN_CANT_LEFT_NEXT_NONE'
+            , 'GREEN_CANT_LEFT_NEXT_FORWARD'
+            , 'GREEN_CANT_LEFT_NEXT_LEFT'
+            , 'GREEN_CANT_LEFT_NEXT_RIGHT'
+
+            , 'RED_CAN_RIGHT_NEXT_NONE'
+            , 'RED_CAN_RIGHT_NEXT_FORWARD'
+            , 'RED_CAN_RIGHT_NEXT_LEFT'
+            , 'RED_CAN_RIGHT_NEXT_RIGHT'
+
+            , 'RED_CANT_RIGHT_NEXT_NONE'
+            , 'RED_CANT_RIGHT_NEXT_FORWARD'
+            , 'RED_CANT_RIGHT_NEXT_LEFT'
+            , 'RED_CANT_RIGHT_NEXT_RIGHT'
+        ]
         self.epsilon = 0.1  # choose wrong action randomly by epsilon
-        self.q_values = pd.DataFrame(np.zeros((4, 4)), columns=self.available_actions)
+        column_names = []
+        column_names.extend(self.available_actions)
+        self.q_values = pd.DataFrame(np.zeros((len(self.available_states), len(column_names))),
+                                     columns=column_names)
         self.learning_count = 1
         self.gamma = 0.1
 
-    def get_prev_q_value(self, state, action):
+    def get_current_q_value(self, state, action):
         state_index = self.available_states.index(state)
         return self.q_values.get_value(state_index, action)
 
@@ -34,24 +58,44 @@ class LearningAgent(Agent):
     def find_max_q_value_in_state(self, state):
         max_value = 0
         for action in self.available_actions:
-            action_value = self.get_prev_q_value(state, action)
+            action_value = self.get_current_q_value(state, action)
             if action_value > max_value:
                 max_value = action_value
 
         return max_value
 
-    def calculate_state(self, inputs):
+    def find_max_q_value_action_in_state(self, state):
+        max_value = 0
+        max_value_action = ""
+        for action in self.available_actions:
+            action_value = self.get_current_q_value(state, action)
+            if action_value >= max_value:
+                max_value = action_value
+                max_value_action = action
+
+        return max_value_action
+
+    def calculate_state(self, inputs, next_waypoint):
+        if next_waypoint == "forward":
+            next_waypoint_postfix = "_NEXT_FORWARD"
+        elif next_waypoint == "left":
+            next_waypoint_postfix = "_NEXT_LEFT"
+        elif next_waypoint == "right":
+            next_waypoint_postfix = "_NEXT_RIGHT"
+        else:
+            next_waypoint_postfix = "_NEXT_NONE"
+
         if inputs.get('light') == 'green':
             oncoming = inputs.get('oncoming')
             if oncoming == None or oncoming == 'left':
-                return 'GREEN_CAN_LEFT'  # None, 'forward', 'right', 'left'
+                return 'GREEN_CAN_LEFT' + next_waypoint_postfix  # None, 'forward', 'right', 'left'
             else:
-                return 'GREEN_CANT_LEFT'  # None, 'forward', 'right'
+                return 'GREEN_CANT_LEFT' + next_waypoint_postfix  # None, 'forward', 'right'
         else:
             if inputs.get('left') != 'forward':
-                return 'RED_CAN_RIGHT'  # None, 'right'
+                return 'RED_CAN_RIGHT' + next_waypoint_postfix  # None, 'right'
             else:
-                return 'RED_CANT_RIGHT'  # None
+                return 'RED_CANT_RIGHT' + next_waypoint_postfix  # None
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
@@ -64,55 +108,28 @@ class LearningAgent(Agent):
         deadline = self.env.get_deadline(self)
 
         # Update state
-        self.state = self.calculate_state(inputs)
+        self.state = self.calculate_state(inputs, self.next_waypoint)
 
-        # TODO: Select action according to your policy
-
-        # my-way policy
-        # action = self.next_waypoint
-
-        # random policy
-        # action = random.choice(self.available_actions)
-
-        # law-abiding policy
-        if self.next_waypoint == 'forward':
-            if self.state == 'GREEN_CAN_LEFT' or self.state == 'GREEN_CANT_LEFT':
-                action = 'forward'
-            else:
-                action = None
-        elif self.next_waypoint == 'left':
-            if self.state == 'GREEN_CAN_LEFT':
-                action = 'left'
-            else:
-                action = None
-        elif self.next_waypoint == 'right':
-            if self.state == 'GREEN_CAN_LEFT' or self.state == 'GREEN_CANT_LEFT' or self.state == 'RED_CAN_RIGHT':
-                action = 'right'
-            else:
-                action = None
+        # Select action according to your policy
+        # use q-learning
+        if random.random() < self.epsilon:
+            # random policy
+            action = random.choice(self.available_actions)
         else:
-            action = None
-
-        # TODO use q-learning
+            # use q_values
+            action = self.find_max_q_value_action_in_state(self.state)
 
         # Execute action and get reward
         reward = self.env.act(self, action)
 
         # Learn policy based on state, action, reward
         next_state_inputs = self.env.sense(self)
-        next_state = self.calculate_state(next_state_inputs)
+        next_waypoint_after_move = self.planner.next_waypoint()
+        next_state = self.calculate_state(next_state_inputs, next_waypoint_after_move)
         learning_rate = 1 / float(self.learning_count)
-        new_q_value = ((1 - learning_rate) * self.get_prev_q_value(self.state, action)) + (
+        new_q_value = ((1 - learning_rate) * self.get_current_q_value(self.state, action)) + (
             learning_rate * (reward + self.gamma * self.find_max_q_value_in_state(next_state)))
-
-        print "state : " + self.state + ", action : " + str(action) + ", next_state : " + str(
-            next_state) + ", new_q_value : " + str(
-            new_q_value)
         self.set_q_value(self.state, action, new_q_value)
-        print self.q_values
-
-        # print self.q_values
-
         self.learning_count += 1
 
         print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}" \
